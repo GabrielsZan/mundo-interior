19f0eaed1491fa60a9105a7634fed4b3d999bc1d
GabrielsZan <gabrielsilvazandonade@gmail.com>
2026-04-06 19:21:49 -0300

    chore: auto-sync 2026-04-06 19:21

---
+++ android/app/src/main/java/com/mundointerior/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mundointerior.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mundointerior.app',
+  appName: 'Mundo Interior',
+  webDir: 'dist'
+};
+
+export default config;
